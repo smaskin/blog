f4fe25e4f6141bfaeb2d82c3ca4a2073fc408425
smaskin <s.maskin@pravo.ru>
2017-03-03 11:41:20 +0300

    api + mongo init

--- app/app.js
+++ app/app.js
@@ -1,62 +1,171 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://mongo:27017');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const path = require('path');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+const app = express();
+app.use(bodyParser.json()); // стандартный модуль, для парсинга JSON в запросах
+app.use(methodOverride()); // поддержка put и delete
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+mongoose.connect('mongodb://mongo:27017');
+const db = mongoose.connection;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+db.on('error', function (err) {
+    console.log('connection error:', err.message);
+});
+db.once('open', function callback() {
+    console.log("Connected to DB!");
+});
+const Schema = mongoose.Schema;
+
+// Schemas
+const Images = new Schema({
+    kind: {
+        type: String,
+        enum: ['thumbnail', 'detail'],
+        required: true
+    },
+    url: {type: String, required: true}
 });
 
-// error handlers
+const Article = new Schema({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    description: {type: String, required: true},
+    images: [Images],
+    modified: {type: Date, default: Date.now}
+});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
+// validation
+Article.path('title').validate(function (v) {
+    return v.length > 5 && v.length < 70;
+});
+
+const ArticleModel = mongoose.model('Article', Article);
+
+module.exports.ArticleModel = ArticleModel;
+
+
+app.get('/api/articles', function (req, res) {
+    return ArticleModel.find(function (err, articles) {
+        if (!err) {
+            return res.send(articles);
+        } else {
+            res.statusCode = 500;
+            console.log('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({error: 'Server error'});
+        }
+    });
+});
+
+app.post('/api/articles', function (req, res) {
+    const article = new ArticleModel({
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        images: req.body.images
+    });
+
+    article.save(function (err) {
+        if (!err) {
+            console.log("article created");
+            return res.send({status: 'OK', article: article});
+        } else {
+            console.log(err);
+            if (err.name == 'ValidationError') {
+                res.statusCode = 400;
+                res.send({error: 'Validation error'});
+            } else {
+                res.statusCode = 500;
+                res.send({error: 'Server error'});
+            }
+            console.log('Internal error(%d): %s', res.statusCode, err.message);
+        }
+    });
+});
+
+app.get('/api/articles/:id', function (req, res) {
+    return ArticleModel.findById(req.params.id, function (err, article) {
+        if (!article) {
+            res.statusCode = 404;
+            return res.send({error: 'Not found'});
+        }
+        if (!err) {
+            return res.send({status: 'OK', article: article});
+        } else {
+            res.statusCode = 500;
+            console.log('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({error: 'Server error'});
+        }
+    });
+});
+
+app.put('/api/articles/:id', function (req, res) {
+    return ArticleModel.findById(req.params.id, function (err, article) {
+        if (!article) {
+            res.statusCode = 404;
+            return res.send({error: 'Not found'});
+        }
+
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.author = req.body.author;
+        article.images = req.body.images;
+        return article.save(function (err) {
+            if (!err) {
+                console.log("article updated");
+                return res.send({status: 'OK', article: article});
+            } else {
+                if (err.name == 'ValidationError') {
+                    res.statusCode = 400;
+                    res.send({error: 'Validation error'});
+                } else {
+                    res.statusCode = 500;
+                    res.send({error: 'Server error'});
+                }
+                console.log('Internal error(%d): %s', res.statusCode, err.message);
+            }
         });
     });
-}
+});
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+app.delete('/api/articles/:id', function (req, res) {
+    return ArticleModel.findById(req.params.id, function (err, article) {
+        if (!article) {
+            res.statusCode = 404;
+            return res.send({error: 'Not found'});
+        }
+        return article.remove(function (err) {
+            if (!err) {
+                console.log("article removed");
+                return res.send({status: 'OK'});
+            } else {
+                res.statusCode = 500;
+                console.log('Internal error(%d): %s', res.statusCode, err.message);
+                return res.send({error: 'Server error'});
+            }
+        });
     });
+});
+
+app.use(function (req, res, next) {
+    res.status(404);
+    console.log('Not found URL: %s', req.url);
+    res.send({error: 'Not found'});
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    console.log('Internal error(%d): %s', res.statusCode, err.message);
+    res.send({error: err.message});
+});
+
+app.get('/ErrorExample', function (req, res, next) {
+    next(new Error('Random error!'));
 });
 
 module.exports = app;
